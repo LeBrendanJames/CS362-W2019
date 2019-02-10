0bfc7a3b86696944766ee6a377c9ad009bf0561a
LeBrendanJames <35370736+LeBrendanJames@users.noreply.github.com>
2019-02-09 20:49:16 -0600

    Adding unit tests
    Done with the four functions I'm testing and the first card (Smithy)

--- projects/jamesbre/dominion/test/unittest3.c
+++ projects/jamesbre/dominion/test/unittest3.c
@@ -6,48 +6,360 @@
 // drawCard
 
 void drawCardUnitTests(){
+    int numTests = 0, numFailures = 0, i = 0;
+    struct gameState * state = NULL;
+    int returnVal = 0;
+
+    printf("\n");
+    printf("TEST FAILURES\n");
+    printf("---------------------------\n");
+
 	// DECK EMPTY TESTS
 	
 		// Empty deck, empty discard, make sure -1 is returned
+        state = malloc(sizeof(struct gameState));
+        state->deckCount[0] = 0;
+        state->discardCount[0] = 0;
+        returnVal = drawCard(0, state);
+        if (returnVal == -1){
+            numTests++;
+        } else {
+            numTests++;
+            numFailures++;
+            printf("No deck/discard to draw from Test FAILED\n");
+            printf("Expected return value of -1. Actual return value: %d\n\n", returnVal);
+        }
+
+
 	
-		// One card in discard
-			// Check discard empty after call to function
-			// Check deck empty after call to function (since the one card will be drawn)
-			// Check hand 1 larger after call to function
-			// Check correct card moved through
+    // One card in discard
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = 0;
+    state->discardCount[0] = 1;
+    state->discard[0][0] = 2;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 13;
+    }
+    returnVal = drawCard(0, state);
+    // Check discard empty after call to function
+    if (state->discardCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in discard - card moved out of discard pile in order to draw Test FAILED\n");
+        printf("Expected discardCount = 0. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    // Check deck empty after call to function (since the one card will be drawn)
+    if (state->deckCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in discard - card moved to hand then drawn - deck count = 0 Test FAILED\n");
+        printf("Expected deckCount = 0. Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand 1 larger after call to function
+    if (state->handCount[0] == 6){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in discard - card moved to hand then drawn - hand count one greater Test FAILED\n");
+        printf("Expected handCount = 6. Actual value: %d\n\n", state->handCount[0]);
+    }
+    // Check correct card moved through
+    if (state->hand[0][5] == 2){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in discard - card moved to deck then drawn - hand has card from discard Test FAILED\n");
+        printf("Expected hand to contain card 2. Actual value of where that card should be: %d\n\n", state->hand[0][5]);
+    }
+    free(state);
+    state = NULL;
 			
-		// Multiple cards in discard
-			// Check discard empty
-			// Check deck is one less than what was in discard
-			// Check hand has one extra card
-			
-		// Max cards in discard
-			// Check discard empty
-			// Check deck is one less that what was in discard
-			// Check hand has one extra card
-			
+    // Multiple cards in discard
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = 0;
+    state->discardCount[0] = 3;
+    state->discard[0][0] = 2;
+    state->discard[0][1] = 3;
+    state->discard[0][2] = 4;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 13;
+    }
+
+    returnVal = drawCard(0, state);
+
+    // Check discard empty
+    if (state->discardCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Multiple cards in discard - cards moved out of discard pile in order to draw Test FAILED\n");
+        printf("Expected discardCount = 0. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    // Check deck is one less than what was in discard
+    if (state->deckCount[0] == 2){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Three cards in discard - cards moved to hand then one drawn - deck count Test FAILED\n");
+        printf("Expected deckCount = 2. Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand has one extra card
+    if (state->handCount[0] == 6){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Three cards in discard - cards moved to deck then drawn - hand has card from discard Test FAILED\n");
+        printf("Expected hand to contain six cards. Actual number: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+    // Max cards in discard
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = 0;
+    state->discardCount[0] = MAX_DECK;
+    for (i = 0; i < MAX_DECK; i++){
+        state->discard[0][i] = 13; // Add all smithy's
+    }
+    state->handCount[0] = 0;
+
+    returnVal = drawCard(0, state);
+
+    // Check discard empty
+    if (state->discardCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in discard - cards moved out of discard pile in order to draw Test FAILED\n");
+        printf("Expected discardCount = 0. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    // Check deck is one less that what was in discard
+    if (state->deckCount[0] == MAX_DECK - 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in discard - cards moved to hand then one drawn - deck count Test FAILED\n");
+        printf("Expected deckCount = MAX_DECK - 1 (399). Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand has one extra card
+    if (state->handCount[0] == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in discard - cards moved to deck then drawn - hand has card from discard Test FAILED\n");
+        printf("Expected hand to contain one card. Actual number: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
 	
 	// DECK NOT EMPTY TESTS
 	
-		// One card in deck
-			// Check deck empty after function call
-			// Check hand has one more card
-			// Check discard unaffected
-			// Check that card moved was correct 
-			
-		// Multiple cards in deck
-			// Check deck has one fewer card
-			// Check hand has one more card
-			// Check discard unaffected
-			
-		// Max cards in deck
-			// Check deck has one fewer card
-			// Check hand has one more card
-			// Check discard unaffected
+    // One card in deck
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = 1;
+    state->deck[0][0] = 12;
+    state->discardCount[0] = 1;
+    state->discard[0][0] = 2;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 13;
+    }
+    returnVal = drawCard(0, state);
+    // Check deck empty after function call
+    if (state->deckCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in deck - card moved to hand - deck count = 0 Test FAILED\n");
+        printf("Expected deckCount = 0. Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand has one more card
+    if (state->handCount[0] == 6){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in deck - card moved to hand Test FAILED\n");
+        printf("Expected hand to contain 6 cards. Actual number: %d\n\n", state->handCount[0]);
+    }
+    // Check discard unaffected
+    if (state->discardCount[0] == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in deck and one in discard - discard unaffected Test FAILED\n");
+        printf("Expected discardCount = 1. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    // Check that card moved was correct
+    if (state->hand[0][5] == 12){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("One card in deck - card moved to hand - hand has correct card Test FAILED\n");
+        printf("Expected hand to contain card 12. Actual value of where that card should be: %d\n\n", state->hand[0][5]);
+    }
+    free(state);
+    state = NULL;
+
+    // Multiple cards in deck
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = 3;
+    state->deck[0][0] = 5;
+    state->deck[0][1] = 6;
+    state->deck[0][2] = 7;
+    state->discardCount[0] = 3;
+    state->discard[0][0] = 2;
+    state->discard[0][1] = 3;
+    state->discard[0][2] = 4;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 13;
+    }
+
+    returnVal = drawCard(0, state);
+
+    // Check deck has one fewer card
+    if (state->deckCount[0] == 2){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Three cards in deck - one card moved to hand - deck count Test FAILED\n");
+        printf("Expected deckCount = 2. Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand has one more card
+    if (state->handCount[0] == 6){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Three cards in deck - one card moved to hand Test FAILED\n");
+        printf("Expected hand to contain 6 cards. Actual number: %d\n\n", state->handCount[0]);
+    }
+    // Check discard unaffected
+    if (state->discardCount[0] == 3){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Three cards in deck and three in discard - discard unaffected Test FAILED\n");
+        printf("Expected discardCount = 3. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+    // Max cards in deck
+    state = malloc(sizeof(struct gameState));
+    state->deckCount[0] = MAX_DECK;
+    for (i = 0; i < MAX_DECK; i++) {
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 0;
+
+    returnVal = drawCard(0, state);
+    // Check deck has one fewer card
+    if (state->deckCount[0] == MAX_DECK - 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in deck - one card moved to hand - deck count Test FAILED\n");
+        printf("Expected deckCount = MAX_DECK - 1 (399). Actual value: %d\n\n", state->deckCount[0]);
+    }
+    // Check hand has one more card
+    if (state->handCount[0] == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in deck - one card moved to hand Test FAILED\n");
+        printf("Expected hand to contain 1 card. Actual number: %d\n\n", state->handCount[0]);
+    }
+    // Check discard unaffected
+    if (state->discardCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Max cards in deck and zero in discard - discard unaffected Test FAILED\n");
+        printf("Expected discardCount = 0. Actual value: %d\n\n", state->discardCount[0]);
+    }
+    free(state);
+    state = NULL;
 
 
 	
 	// Check that nothing else about gameState has changed
+    state = malloc(sizeof(struct gameState));
+    struct gameState * stateCopy = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    stateCopy->numPlayers = 3;
+    state->handCount[0] = 1;
+    stateCopy->handCount[0] = 1;
+    state->hand[0][0] = curse;
+    stateCopy->hand[0][0] = curse;
+    state->discardCount[0] = 1;
+    stateCopy->discardCount[0] = 1;
+    state->discard[0][0] = curse;
+    stateCopy->discard[0][0] = curse;
+    state->deckCount[0] = 1;
+    stateCopy->deckCount[0] = 1;
+    state->deck[0][0] = curse;
+    stateCopy->deck[0][0] = curse;
+    shuffle(0, state);
+    if (state->numPlayers == stateCopy->numPlayers){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("No Change in numPlayers Variable Test FAILED\n");
+        printf("--> state numPlayers = %d, stateCopy numPlayers = %d\n\n", state->numPlayers, stateCopy->numPlayers);
+    }
+    if (state->hand[0][0] == stateCopy->hand[0][0]){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("No Change in hand[0][0] Variable Test FAILED\n");
+        printf("--> state hand[0][0] = %d, stateCopy hand[0][0] = %d\n\n", state->hand[0][0], stateCopy->hand[0][0]);
+    }
+    if (state->deck[0][0] == stateCopy->deck[0][0]){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("No Change in deck[0][0] Variable Test FAILED\n");
+        printf("--> state deck[0][0] = %d, stateCopy deck[0][0] = %d\n\n",
+               state->deck[0][0], stateCopy->deck[0][0]);
+    }
+    free(state);
+    state = NULL;
+    free(stateCopy);
+    stateCopy = NULL;
+
+
+
+    printf("\nSUMMARY\n");
+    printf("---------------------------\n");
+    printf("Tests Run: %d, Tests Failed: %d\n\n", numTests, numFailures);
 }
 
 int main(int argc, char* argv[]){
--- projects/jamesbre/dominion/test/unittest4.c
+++ projects/jamesbre/dominion/test/unittest4.c
@@ -7,25 +7,384 @@
 
 
 void endTurnUnitTests(){
+    int numTests = 0, numFailures = 0, i = 0;
+    struct gameState * state = NULL;
+    int returnVal = 0;
+
+    printf("\n");
+    printf("TEST FAILURES\n");
+    printf("---------------------------\n");
+
 	// NOT END OF GAME SCENARIO
-		// Check that players hand is empty after running
-		
-		// Check that players discard increased by # of card in hand
-		
-		// Check that cards moved from hand to discard are correct
-		
-		// check that all the rest of the state looks correct
-		
-		// Check that next player is correct 
-			// (I'm pretty sure there is an off-by-one error here)
-		
-		// Check that next player draws hand correctly (5 cards, right?)
-		
-		// check that coins are correct (whatever correct means)
+	// Check that players hand is empty after running (starts with empty hand)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 0;
+    returnVal = endTurn(state);
+    if (state->handCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with no cards in hand - Test FAILED\n");
+        printf("Expected cards in hand to remain = 0. Actual handCount: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+    // Check that players hand is empty after running (starts with cards in hand)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 12;
+    }
+    returnVal = endTurn(state);
+    if (state->handCount[0] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with 5 cards in hand - Test FAILED\n");
+        printf("Expected cards in hand to be = 0. Actual handCount: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+	// Check that players discard increased by # of card in hand
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 5;
+    for (i = 0; i < state->handCount[0]; i++){
+        state->hand[0][i] = 12;
+    }
+    returnVal = endTurn(state);
+    if (state->discardCount[0] == 5){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with 5 cards in hand - discard check - Test FAILED\n");
+        printf("Expected cards in discard to be = 5. Actual discardCount: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+	// Check that cards moved from hand to discard are correct
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 3;
+    state->hand[0][0] = 4;
+    state->hand[0][1] = 5;
+    state->hand[0][2] = 6;
+    returnVal = endTurn(state);
+    if (state->discard[0][0] == 4 && state->discard[0][1] == 5 && state->discard[0][2] == 6){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with 3 cards in hand - corrrect cards end up in discard - Test FAILED\n");
+        printf("Expected cards in discard to be [4, 5, 6]. Actual discard: [%d, %d, %d]\n\n",
+               state->discard[0][0], state->discard[0][1], state->discard[0][2]);
+    }
+    free(state);
+    state = NULL;
+
+	// Check that cards moved don't overwrite anything already in discard
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 2;
+    state->discard[0][0] = 0;
+    state->discard[0][1] = 17;
+    state->handCount[0] = 3;
+    state->hand[0][0] = 4;
+    state->hand[0][1] = 5;
+    state->hand[0][2] = 6;
+    returnVal = endTurn(state);
+    if (state->discardCount[0] == 5){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with 3 cards in hand and 2 in discard - discard ends up with 5 - Test FAILED\n");
+        printf("Expected discardCount: 5. Actual discardCount: %d\n\n", state->discardCount[0]);
+    }
+    if (state->discard[0][0] == 0 && state->discard[0][1] == 17){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn with 3 cards in hand and two in discard - previously discarded not overwritten - Test FAILED\n");
+        printf("Expected cards at bottom of discard to be [0, 17]. Actual discard: [%d, %d]\n\n",
+               state->discard[0][0], state->discard[0][1]);
+    }
+    free(state);
+    state = NULL;
+
+	// check that all the rest of the state looks correct
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 2;
+    state->discard[0][0] = 0;
+    state->discard[0][1] = 17;
+    state->handCount[0] = 3;
+    state->hand[0][0] = 4;
+    state->hand[0][1] = 5;
+    state->hand[0][2] = 6;
+    state->deckCount[2] = 3;
+    for (i = 0; i < state->deckCount[2]; i++){
+        state->deck[2][i] = 13;
+    }
+    state->discardCount[2] = 3;
+    for (i = 0; i < state->discardCount[2]; i++){
+        state->discard[2][i] = 4;
+    }
+    state->handCount[2] = 0;
+
+    returnVal = endTurn(state);
+
+    if (state->deckCount[0] == 5){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Deck unaffected by endTurn - Test FAILED\n");
+        printf("Expected deckCount of 5. Actual count: %d\n\n", state->deckCount[0]);
+    }
+    if (state->numPlayers == 3){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("numPlayers unaffected by endTurn - Test FAILED\n");
+        printf("Expected numPlayers of 3. Actual count: %d\n\n", state->numPlayers);
+    }
+    if (state->numPlayers == 3){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("numPlayers unaffected by endTurn - Test FAILED\n");
+        printf("Expected numPlayers of 3. Actual count: %d\n\n", state->numPlayers);
+    }
+    if (state->deckCount[2] == 3 && state->discardCount[2] == 3 && state->handCount[2] == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("Other player (not next to act) unaffected by endTurn - Test FAILED\n");
+        printf("Expected player #3 deckCount = 3, discardCount = 3, handCount = 0\n");
+        printf("Ended function with deckCount = %d, discardCount = %d, handCount = %d\n\n",
+               state->deckCount[2], state->discardCount[2], state->handCount[2]);
+    }
+    free(state);
+    state = NULL;
+
+	// Check that next player is correct (player 0 -> player 1)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 0;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 3;
+    state->hand[0][0] = 4;
+    state->hand[0][1] = 5;
+    state->hand[0][2] = 6;
+    returnVal = endTurn(state);
+    if (state->whoseTurn == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - next player (player 0 -> player 1) - Test FAILED\n");
+        printf("Expected next player = 1. Actual next player: %d\n\n", state->whoseTurn);
+    }
+    free(state);
+    state = NULL;
+
+    // Check that next player is correct (player 2 -> player 0)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 2;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 3;
+    state->hand[0][0] = 4;
+    state->hand[0][1] = 5;
+    state->hand[0][2] = 6;
+    returnVal = endTurn(state);
+    if (state->whoseTurn == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - next player (player 2 -> player 0 in 3-person game) - Test FAILED\n");
+        printf("Expected next player = 0. Actual next player: %d\n\n", state->whoseTurn);
+    }
+    free(state);
+    state = NULL;
+
+	// Check that next player draws hand correctly (5 cards, right?)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 2;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 0;
+    returnVal = endTurn(state);
+    if (state->handCount[0] == 5){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - next player draws hand correctly - Test FAILED\n");
+        printf("Expected next player handCount = 5. Actual next player handCount: %d\n\n", state->handCount[0]);
+    }
+    free(state);
+    state = NULL;
+
+	// check that coins and other stuff are correct (whatever correct means)
+    state = malloc(sizeof(struct gameState));
+    state->numPlayers = 3;
+    state->whoseTurn = 2;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 0;
+    state->outpostPlayed = 1;
+    state->phase = 3;
+    state->numActions = 0;
+    state->coins = 12;
+    state->numBuys = 0;
+    state->playedCardCount = 5;
+    returnVal = endTurn(state);
+    if (state->outpostPlayed == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - outpostPlayed reset - Test FAILED\n");
+        printf("Expected outpostPlayed = 0. Actual outpostPlayed: %d\n\n", state->outpostPlayed);
+    }
+    if (state->phase == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - phase reset - Test FAILED\n");
+        printf("Expected phase = 0. Actual phase: %d\n\n", state->phase);
+    }
+    if (state->numActions == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - numActions reset - Test FAILED\n");
+        printf("Expected numActions = 1. Actual numActions: %d\n\n", state->numActions);
+    }
+    if (state->coins == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - coins reset - Test FAILED\n");
+        printf("Expected coins = 0. Actual coins: %d\n\n", state->coins);
+    }
+    if (state->numBuys == 1){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - numBuys reset - Test FAILED\n");
+        printf("Expected numBuys = 1. Actual numBuys: %d\n\n", state->numBuys);
+    }
+    if (state->playedCardCount == 0){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - playedCardCount reset - Test FAILED\n");
+        printf("Expected playedCardCount = 0. Actual playedCardCunt: %d\n\n", state->numBuys);
+    }
+    free(state);
+    state = NULL;
 		
 	
 	// END OF GAME SCENARIO
-		// (Per comment in .h file, this shouldn't advance player, but there doesn't seem to be any code that implements that)
+	// (Per comment in .h file, this shouldn't advance player, but there doesn't seem to be any code that implements that)
+    state = malloc(sizeof(struct gameState));
+    state->supplyCount[province] = 0;
+    state->numPlayers = 3;
+    state->whoseTurn = 2;
+    state->deckCount[0] = 5;
+    for (i = 0; i < state->deckCount[0]; i++){
+        state->deck[0][i] = 13;
+    }
+    state->discardCount[0] = 0;
+    state->handCount[0] = 0;
+    returnVal = endTurn(state);
+    if (state->whoseTurn == 2){
+        numTests++;
+    } else {
+        numTests++;
+        numFailures++;
+        printf("End Turn - player should not advance if game is over - Test FAILED\n");
+        printf("Expected whoseTurn to remain 2. Actual whoseTurn: %d\n\n", state->whoseTurn);
+    }
+    free(state);
+    state = NULL;
+
+
+
+    printf("\nSUMMARY\n");
+    printf("---------------------------\n");
+    printf("Tests Run: %d, Tests Failed: %d\n\n", numTests, numFailures);
 }
 
 
