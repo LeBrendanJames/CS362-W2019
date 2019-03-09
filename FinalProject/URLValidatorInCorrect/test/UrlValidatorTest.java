b81f46c605f1352d2afe267d1939388c06fb1605
LeBrendanJames <35370736+LeBrendanJames@users.noreply.github.com>
2019-03-09 15:02:25 -0600

    Implemented I/O for manual tests
    Test runs in a while loop that takes an input URL, prints result, then loops to top again. Exit by typing "quit".

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,9 @@
 
 
 import junit.framework.TestCase;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -19,10 +22,33 @@ public class UrlValidatorTest extends TestCase {
 
    
    
-   public void testManualTest()
+   public void testManualTest() throws IOException
    {
-//You can use this function to implement your manual testing	   
+	//You can use this function to implement your manual testing
+	   String inputStr;
+       long options =
+               UrlValidator.ALLOW_2_SLASHES
+                   + UrlValidator.ALLOW_ALL_SCHEMES
+                   + UrlValidator.NO_FRAGMENTS;
+       UrlValidator urlVal = new UrlValidator(null, null, options);
+       
+	   do {
+		   System.out.print("Enter the URL you would like to test: ");
+		   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		   inputStr = in.readLine();
+		   
+		   // Run input through URLValidator
+		   if (inputStr.contentEquals("quit") == false) {
+			   boolean result = urlVal.isValid(inputStr);
+			   // Print results from URLValidator
+			   System.out.println("URLValidator returned " + result + " for input URL: " + inputStr);
+		   }
+		   
+		   
+	   } while (inputStr.contentEquals("quit") == false);
 	   
+	   
+	   System.out.print("Quitting manual testing");
    }
    
    
